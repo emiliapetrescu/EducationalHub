3ee7be59801ce0ac40e92794f5a65800aadaaff6
Emilia Petrescu <emiliaflorentina.petrescu@gmail.com>
2024-01-10 15:56:30 +0200

    update budget table - total incomes, total expenses, balance - functional

--- mainApp/finances/buget.js
+++ mainApp/finances/buget.js
@@ -8,8 +8,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const mainDropdown = getElement('main-expenses-dropdown');
     const nestedDropdown = getElement('nested-expenses-dropdown');
     const initialOptions = Array.from(nestedDropdown.options);
-    const TOTAL_INCOMES_BTN = getElement('total-income-btn');
-    const RESULT_DISPLAY = getElement('resultDisplay');
+    const totalIncomes = getElement('total-incomes');
+    const totalExpenses = getElement('total-expenses');
+    const balanceResult = getElement('balance');
 
     const combineHandler = () => {
         nestedDropdown.innerHTML = '';
@@ -30,62 +31,74 @@ document.addEventListener('DOMContentLoaded', () => {
     const addRecordHandler = (inputIds, tableBody, containerId) => {
         let userInput = true;
         const newRow = tableBody.insertRow();
-    
+
         inputIds.forEach((inputId, index) => {
             const inputElement = getElement(inputId);
-    
+
             const cell = newRow.insertCell(index);
-    
+
             if (inputElement.tagName === 'SELECT') {
                 cell.textContent = inputElement.options[inputElement.selectedIndex].text;
-                } else {
-                    cell.textContent = inputElement.value;
-                }
-        });
-    
-            if (userInput) {
-                getElement(containerId).style.display = 'block';
+            } else {
+                cell.textContent = inputElement.value;
             }
+        });
+
+        if (userInput) {
+            getElement(containerId).style.display = 'block';
+        }
     };
-    
+
     const addIncomeRecord = () => {
         const inputIds = ['income-type', 'income-frequency', 'income-amount', 'income-date'];
         addRecordHandler(inputIds, incomeTableBody, 'income-table-container');
+        updateMonthlyBudget();
     };
 
     const addExpenseRecord = () => {
         const inputIds = ['main-expenses-dropdown', 'nested-expenses-dropdown', 'expense-frequency', 'expense-amount', 'expenses-date'];
-    
+
         addRecordHandler(inputIds, expenseTableBody, 'expense-table-container');
+        updateMonthlyBudget();
     };
-    
+
+    const updateMonthlyBudget = () => {
+        const totalIncomesValue = calculateTotalIncomes();
+        const totalExpensesValue = calculateTotalExpenses();
+
+        totalIncomes.value = totalIncomesValue;
+        totalExpenses.value = totalExpensesValue;
+
+        const balanceValue = totalIncomesValue - totalExpensesValue;
+        balanceResult.value = balanceValue;
+    };
+
+    const calculateTotalIncomes = () => {
+        const incomeRows = Array.from(incomeTableBody.getElementsByTagName('tr'));
+
+        const totalIncomes = incomeRows.reduce((sum, row) => {
+            const incomeAmountCell = row.cells[2]; //modify for dynamic values
+            const incomeAmount = parseFloat(incomeAmountCell.textContent) || 0;
+            return sum + incomeAmount;
+        }, 0);
+
+        return totalIncomes;
+    };
+
+    const calculateTotalExpenses = () => {
+        const expenseRows = Array.from(expenseTableBody.getElementsByTagName('tr'));
+
+        const totalExpenses = expenseRows.reduce((sum, row) => {
+            const expenseAmountCell = row.cells[3]; //modify for dynamic values
+            const expenseAmount = parseFloat(expenseAmountCell.textContent) || 0;
+            return sum + expenseAmount;
+        }, 0);
+
+        return totalExpenses;
+    };
+
     mainDropdown.addEventListener('change', combineHandler);
     getElement('add-income-btn').addEventListener('click', addIncomeRecord);
     getElement('add-expenses-btn').addEventListener('click', addExpenseRecord);
-
-    const totalIncomes = () => {
-        const getValue = (id) => {
-            const value = parseInt(getElement(id).value);
-            return isNaN(value) ? 0 : value;
-        };
-
-        const SALARY1 = getValue('salary_1');
-        const SALARY2 = getValue('salary_2');
-        const OTHER_INCOMES = getValue('otherIncomes');
-
-        if (SALARY1 === 0 && SALARY2 === 0 && OTHER_INCOMES === 0) {
-            alert('We need at least one value to be a number.');
-        } else {
-            return SALARY1 + SALARY2 + OTHER_INCOMES;
-        }
-    };
-
-    TOTAL_INCOMES_BTN.addEventListener('click', (event) => {
-        event.preventDefault();
-        console.log('button clicked');
-        const result = totalIncomes();
-        if (result !== undefined) {
-            RESULT_DISPLAY.value = result;
-        }
-    });
+    updateMonthlyBudget();
 });
